1687b6bcae605be96e1836041ca541587f739c7d
Daniel Suciu <suciudani@gmail.com>
2018-06-18 16:12:14 +0300

    Exercises1

---
+++ js/ex1.js
@@ -0,0 +1,32 @@
+var i, j;
+for(i=5;i>=1;i--)
+{
+    for(j=1;j<=i;j++)
+    {
+        document.write('-');
+    }
+    document.write('<br/>');
+}
+
+
+// var n = '';
+// for(var i = 10; i > 0; i --){
+//     n = n + '-';
+//     if (i > 10) {
+//         break;
+//     }
+//     console.info(n);
+//     // document.write(n + "<br>");
+// }
+
+
+// var x,y,n;
+// for(x=1; x <=10; x++)
+// {
+//     for (y=1; y < x; y++)
+//     {
+//         n=n+("-");
+//     }
+//     console.log(n);
+//     n='';
+// }
